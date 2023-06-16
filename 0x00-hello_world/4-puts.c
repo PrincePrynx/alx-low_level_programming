41fc792a83f511d192f5fbb5983898f8491997ef
PrincePrynx <poetykprynx@gmail.com>
2023-06-15 19:12:11 -0700

    Using The Put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - the starting point
+* Description: printing literals the 'puts' function
+* Return: always ends with 0
+*/
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
