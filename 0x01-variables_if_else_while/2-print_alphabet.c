2d40548a73666b6783812bc87a688f140185697d
PrincePrynx <poetykprynx@gmail.com>
2023-06-16 21:02:38 -0700

    lowercase converter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Starting point
+*Description: ‘Prints the alphabet in lowercase’
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+	while
+		(letter <= 'z') {
+			putchar(letter);
+		c++;
+		}
+
+	putchar('\n');
+	return (0);
+}
