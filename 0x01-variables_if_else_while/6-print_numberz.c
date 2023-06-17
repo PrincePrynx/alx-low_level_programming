38833692e439a39246b560e493b4e8a71cde2e68
PrincePrynx <poetykprynx@gmail.com>
2023-06-16 22:26:23 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - starting point
+ * Description: prints numbers
+ * Return: set at 0 (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+	printf("%i", i);
+
+printf("\n");
+return (0);
+}
