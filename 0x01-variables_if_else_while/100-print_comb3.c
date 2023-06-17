21bede5ee23a670ff72841071a347187af7bce20
PrincePrynx <poetykprynx@gmail.com>
2023-06-16 23:21:41 -0700

    Magic Combo Numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - starting point
+*Description: prints all possible different combinations of 2 digits
+*Return: set at 0 (success)
+*/
+int main(void)
+{
+	int c = 0;
+	int FD;
+	int LD;
+
+	while (c <= 99)
+	{
+		FD= (c / 10 + '0');
+		LD = (c % 10 + '0');
+
+		if (FD < LD)
+		{
+			putchar(FD);
+			putchar(LD);
+
+			if (c != 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		c++;
+	}
+putchar('\n');
+return (0);
+}
