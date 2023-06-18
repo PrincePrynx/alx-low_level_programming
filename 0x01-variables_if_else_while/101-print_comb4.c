924a0cb980a7f653f623c00fd09cdef4c3ec232b
PrincePrynx <poetykprynx@gmail.com>
2023-06-18 07:43:38 -0700

    number combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - starting point
+*Description: prints all possible different combinations of two 2 digits
+ *Return: set at 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+	int fD;
+	int sD;
+	int lD;
+
+	while (c <= 999)
+		fD = (c / 100 + '0');
+		sD = (c / 10 % 10 + '0');
+		lD = (c % 10 + '0');
+
+		if ((fD < sD) && (sD < lD))
+			putchar(fD);
+			putchar(sD);
+			putchar(lD);
+
+			if (c != 789)
+				putchar(',');
+				putchar(' ');
+		c++;
+
+putchar('\n');
+return (0);
+}
