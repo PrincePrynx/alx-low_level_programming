0ea997ee2867a6c75c2b3ae662c182371f0f7682
PrincePrynx <poetykprynx@gmail.com>
2023-06-22 05:15:13 -0700

    10 sets of 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 0 - 14, ten times
+*
+*Return: void
+*/
+
+void more_numbers(void)
+{
+int c, m;
+
+for (m = 1; m <= 10; m++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c >= 10)
+_putchar ('1');
+_putchar (c % 10 + '0');
+
+}
+_putchar ('\n');
+}
+}
