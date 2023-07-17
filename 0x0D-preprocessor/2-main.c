051b1bc559af4fe24f7dab6cb148b5e01f54cb40
PrincePrynx <poetykprynx@gmail.com>
2023-07-17 16:13:52 -0700

    MACROS

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - prints the name of the file
-*
-*Return: always 0
+*main - Entry point
+*Return: 0
 */
-
 
 int main(void)
 {
+	printf("%s\n", __FILE__);
 
-	printf("%s/n", __FILE__);
 	return (0);
 }
