73b5a41cfcc482723b9a4d2da0657a3f6d780e2a
PrincePrynx <poetykprynx@gmail.com>
2023-07-17 07:25:00 -0700

    MACROS

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,11 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
+
+/**
+*File: O-object_like_macro
+*
+*Desc: header file
+*/
 
 #define SIZE 1024
 
